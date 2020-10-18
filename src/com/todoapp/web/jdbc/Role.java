8c44d9eb21b39c844c862e3402eee886c03b58fd
SebastienML <sebastien.lejeune6@gmail.com>
2020-10-18 16:07:03 +0200

    Entities generation

---
+++ src/com/todoapp/web/jdbc/Role.java
@@ -0,0 +1,36 @@
+package com.todoapp.web.jdbc;
+
+public class Role {
+	private int id;
+	private String libelle;
+
+	public Role(int id, String libelle) {
+		this.id = id;
+		this.libelle = libelle;
+	}
+
+	public Role(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", libelle=" + libelle + "]";
+	}
+}
---
+++ src/com/todoapp/web/jdbc/User.java
@@ -0,0 +1,92 @@
+package com.todoapp.web.jdbc;
+
+public class User {
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	private Role idrole;
+	private Class idclass;
+
+	public User(String username, String password, Role idrole) {
+		this.username = username;
+		this.password = password;
+		this.idrole = idrole;
+	}
+
+	public User(String username, String password, Role idrole, Class idclass) {
+		this.username = username;
+		this.password = password;
+		this.idrole = idrole;
+		this.idclass = idclass;
+	}
+
+	public User(String username, String password, Role idrole, Class idclass, String email) {
+		this.username = username;
+		this.password = password;
+		this.idrole = idrole;
+		this.idclass = idclass;
+		this.email = email;
+	}
+
+	public User(String username, String password, Role idrole, String email) {
+		this.username = username;
+		this.password = password;
+		this.idrole = idrole;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Role getIdrole() {
+		return idrole;
+	}
+
+	public void setIdrole(Role idrole) {
+		this.idrole = idrole;
+	}
+
+	public Class getIdclass() {
+		return idclass;
+	}
+
+	public void setIdclass(Class idclass) {
+		this.idclass = idclass;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
+	}
+
+}
